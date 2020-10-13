44961a6c513942cd7dfd7042c481faddb38e2afa
Ian Hinder <ian.hinder@manchester.ac.uk>
2020-10-13 23:37:15 +0100

    TestFEM.cpp: Add clamped filament from Turek Hron example with a downward force

--- src/TestFEM.cpp
+++ src/TestFEM.cpp
@@ -19,12 +19,61 @@
 // Includes
 #include "../inc/FEMBody.h"
 
+
+class TestFEMClass : public FEMBodyListenerClass
+{
+	public:
+
+	TestFEMClass() {
+		double length = 0.35;
+		nNodes = static_cast<int>(floor(length / Dx())) + 1;
+		cout << "nNodes = " << nNodes << endl;
+
+		for (int i = 0; i < nNodes; i++) {
+			pos.push_back({i * Dx(), 0});
+			vel.push_back({0,0});
+		}
+		time = 0;
+	}
+
+	double Dt() {return 0.00001;};
+	double t() {return time;};
+	double Dx() {return 1./250;};
+	double Dm() {return 1.0;};
+	int bodyID() {return 0;};
+	void setNodePosition(int nodeID, array<double, dims> newpos) {/*cout << "Setting node " << nodeID << " position to " << newpos[0] << "," << newpos[1] << endl; */pos[nodeID] = newpos;};
+	void setNodeVelocity(int nodeID, array<double, dims> newvel) {vel[nodeID] = newvel;};
+	int numNodes() {return nNodes;};
+	double epsilon(int /*nodeID*/) {return 2;};
+	array<double, dims> force(int /*nodeID*/) {return array<double, dims>({0,-0.01});}
+
+	void advanceTime() {time += Dt();};
+	array<double, dims> getNodePosition(int nodeID) {return pos[nodeID];};
+
+	double getTime() {return time;};
+	private:
+	
+	vector<array<double, dims>> pos;
+	vector<array<double, dims>> vel;
+	int nNodes;
+	double time;
+};
+
 // ***** Main function ***** //
 int main() {
 
 	cout << "*** INITIALISING TestFEMMain ***" << endl;
 
-	FEMBodyClass  fBody;
+	TestFEMClass testFEM;
+
+	FEMBodyClass  fBody(&testFEM, {0,0}, {0.35,0.02}, 0, "20", "CLAMPED", 10000.0, 1.4e6);
+	
+	for (int i = 0; i < 20; i++) {
+		auto pos = testFEM.getNodePosition(87);
+		cout << "Tip position at time " << testFEM.getTime() << " : " << pos[0] << " " << pos[1] << endl;
+		fBody.dynamicFEM();
+		testFEM.advanceTime();
+	}
 
 	cout << "TestFEMMain done." << endl;
 }
